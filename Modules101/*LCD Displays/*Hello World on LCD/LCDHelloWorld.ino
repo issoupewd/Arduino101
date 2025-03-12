fdb0c4ce095d07ad898a1be16473e0153f3fff0a
issoupewd <128074111+issoupewd@users.noreply.github.com>
2025-03-12 01:55:26 +0100

    Create LCDHelloWorld.ino

---
+++ Modules101/*LCD Displays/*Hello World on LCD/LCDHelloWorld.ino
@@ -0,0 +1,25 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(2,1);
+  lcd.print("ARDUINO WORKSHOP!");
+   lcd.setCursor(2,2);
+  lcd.print("CELEC  Community");
+   lcd.setCursor(5,3);
+  lcd.print("TEST TEST....");
+}
+
+
+void loop()
+{
+}
